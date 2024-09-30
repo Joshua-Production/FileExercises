16a7e73da2ac4a260566730f284314154d66c742
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-09-30 10:42:45 -0500

    Add project files.

---
+++ FileExercises/SerializeIO.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Write a Serialize and DeSerialize function for Contact struct.
+ * Serialize should write the contents of the object into a file.
+ * DeSeralize should read the contents of a file and assign them to the object's variables.
+ */
+
+namespace FileExercises
+{
+    
+    struct Contact
+    {
+        public string name;
+        public string email;
+        public int id;
+        public Contact(string name, string email, int id)
+        {
+            this.name = name;
+            this.email = email;
+            this.id = id;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine(name = " | " + email + " | " + id);
+        }
+    }
+    internal class SerializeIO
+    {
+        public void Run()
+        {
+            // Make 3 contacts 
+            Contact bob = new Contact("Bob", "bob@email.com", 1234);
+            Contact fred = new Contact("Fred", "fred@email.com", 2345);
+            Contact jane = new Contact("Jane", "Jane@email.com", 3456);
+
+            // Write each contact to a file
+            bob.Serialize(@"contacts\bob.txt");
+            fred.Serialize(@"contacts\fred.txt");
+            jane.Serialize(@"contacts\jane.txt");
+
+            // Clear out contacts
+            bob = new Contact();
+            fred = new Contact();
+            jane = new Contact();
+
+            // Load contacts from file
+            bob.DeSerialize(@"contacts\bob.txt");
+            fred.DeSerialize(@"contacts\fred.txt");
+            jane.DeSerialize(@"contacts\jane.txt");
+
+            // Print contacts
+            bob.Print();
+            fred.Print();
+            jane.Print();
+        }
+
+
+
+        
+    }
+}
---
+++ FileExercises/TextIO.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileExercises
+{
+    internal class TextIO
+    {
+
+        public void Run()
+        {
+            string path = @"text.txt";
+            // Write file
+            if (!File.Exists(path))
+            {
+                try
+                {
+                    // this make the new path
+                    StreamWriter writer = new StreamWriter(path);
+                    // what i want it to write 
+                    writer.WriteLine("Hello");
+                    writer.WriteLine("And");
+                    writer.WriteLine("Welcome!");
+                    writer.Close();
+                    // Dispose of the object
+                    try
+                    {
+                        writer.Dispose();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            // Read file
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string str;
+                    while ((str = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(str);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
